757ebd193f29e1e3e7656845a8e3be693d211dae
cforcomputer <patjobri@gmail.com>
2019-07-15 06:38:42 -0700

    final submission

---
+++ lesson-2/Patrick_OBrien_SUBMISSION/PCE_02/PCE_Starter/Program.cs
@@ -0,0 +1,295 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PCE_StarterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using_DotNets_Stack uds = new Using_DotNets_Stack();
+            //uds.RunExercise();
+
+            //Reversing_User_Input rui = new Reversing_User_Input();
+            //rui.RunExercise();
+
+            //Basic_Generic_Test_Code bgtc = new Basic_Generic_Test_Code();
+            //bgtc.RunExercise();
+
+            Basic_AbsValComparer_Test_Code bavctc = new Basic_AbsValComparer_Test_Code();
+            bavctc.RunExercise();
+
+            // add more here, as you need
+        }
+    }
+
+    class What_Is_A_Stack 
+    {
+        // Put your answer here (in comments)
+        /// A stack is a container of objects that are inserted and removed according to the last-in first-out
+        /// LIFO principle. In the pushdown stacks, only two operations are allowed: push the item into the stack,
+        /// and pop the item out of the stack. 
+        /// A stack is a limited access data structure - elements can be removed and added only from the top. 
+        /// push = adds an item to the top of the stack
+        /// pop = removes an item from the top
+        /// A stack is a recursive data structure 
+        /// it is either empty, or consists of a top, and the rest is a stack.
+        /// 
+        /// Applications:
+        /// Reversing a word
+        /// The undo mechanism in text editors
+        /// Backtracking -- The process when you access the most recent element in a series of elements. 
+        /// Therefore at each point you on a stack all possible choice. Backtracking means popping a next 
+        /// choice from the stack.
+        /// Language processing -- space for parameters and local variables is created internally using a stack.
+        /// Compiler's syntax check for matching braces is implemented using a stack.
+        /// Support for recursion.
+        /// 
+        /// Implementations
+        /// Array based, Linked List
+        /// 
+        // Answer to 2. 
+        /// Push 1, 2, and 3 onto a stack.
+        /// 1-2-3
+        /// Pop the stack twice.
+        /// 1
+        /// Then push 4 and 5
+        /// 1-4-5
+        /// The top end is the 5, since it was the last number (presumptively) to be added to the stack.
+        /// Each new number placed on the stack ends up on top of the last number to be added.
+    }
+
+
+    class Car
+    {
+    }
+
+    class Using_DotNets_Stack 
+    {
+        // Your comment explaining the running time of
+        // Push goes here
+        /// When talking about the O(1) operation and O(N) operations:
+        /// What does O(1) operation apply to in a stack?
+        /// A: Peek, push, and pop
+        /// There is not much processing involved in these cases, and they are merely returning a singular primitive.
+        /// What does N refer to in the context of Push?
+        /// A: N refers to the number of items in the stack. This would apply if we are using (ex: loop)
+        /// to pass through the stack to find a specific number.
+
+		// Make sure that "using System.Collections;" is at the top of this file
+        public void RunExercise()
+        {
+            // Create a stack
+            // Push numbers 1, 2 and 3
+            // Pop off the values and print them as you do so
+            // Make sure to use push, pop, and peek methods in code
+            // Less than 25 lines
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            // 1, 2, 3
+            
+            for (int i = 0; i < stack.Count; i++)
+            {
+                // Print the stack and then pop that value in the stack.
+                Console.WriteLine("Pop\t\t{0}", stack.Pop());
+            }
+            Console.WriteLine("Peek\t\t{0}", stack.Peek());
+        }
+    }
+
+    class Reversing_User_Input
+    {
+        public void RunExercise()
+        {
+            // Create generic stack object
+            Stack<int> stack = new Stack<int>();
+            int readLine = 0;
+
+            while (readLine > -1)
+            {
+                Console.WriteLine("Please type a number");
+                Console.WriteLine("Please type a negative number to stop");
+                readLine = Convert.ToInt32(Console.ReadLine());
+                // Check to see if negative or not
+                if (readLine > -1)
+                {
+                    stack.Push(readLine);
+                }
+            }
+            Console.WriteLine("Here's what you typed, backwards:");
+
+            // Print contents of array backwards
+            for (int i = 0; i < stack.Count; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            Console.WriteLine(stack.Peek());
+        }
+    }
+
+    class Basic_Generic_Test_Code
+    {
+        public void RunExercise()
+        {
+            //These lines of code have been commented out.
+            // They won't compile until you implement the generic
+            // BasicGeneric class.
+
+            // Note that you should NOT modify this code at all:
+
+            //////////////// ints /////////////////////////////////////////////
+            Console.WriteLine("=============================");
+            Console.WriteLine("Test for INT");
+            Console.WriteLine("=============================\n");
+
+            BasicGeneric<int> basic_int = new BasicGeneric<int>();
+            int intValueToStore = 100;
+            basic_int.SetItem(intValueToStore);
+
+            Console.WriteLine("Stored {0}, Print() tells us:", intValueToStore);
+            basic_int.Print();
+
+            double intCheckVal = basic_int.GetItem();
+            Console.WriteLine("Using Get, got back: {0}", intCheckVal);
+            if (intCheckVal != intValueToStore)
+                Console.WriteLine("\tERROR: checkVal is not {0}", intValueToStore);
+            else
+                Console.WriteLine("\tGetItem appeared to work!");
+
+            //////////////// doubles //////////////////////////////////////////
+            Console.WriteLine("\n=============================");
+            Console.WriteLine("Test for DOUBLE");
+            Console.WriteLine("=============================\n");
+            BasicGeneric<double> basic_double = new BasicGeneric<double>();
+            double doubleValueToStore = 3.14159;
+            basic_double.SetItem(doubleValueToStore);
+
+            Console.WriteLine("Stored {0}, Print() tells us:", doubleValueToStore);
+            basic_double.Print();
+
+            double doubleCheckVal = basic_double.GetItem();
+            Console.WriteLine("Using Get, got back: {0}", doubleCheckVal);
+            if (doubleCheckVal != doubleValueToStore)
+                Console.WriteLine("\tERROR: checkVal is not {0}", doubleValueToStore);
+            else
+                Console.WriteLine("\tGetItem appeared to work!");
+
+            //////////////// strings //////////////////////////////////////////
+            Console.WriteLine("\n=============================");
+            Console.WriteLine("Test for STRING");
+            Console.WriteLine("=============================\n");
+
+            BasicGeneric<string> basic_string = new BasicGeneric<string>();
+            string stringValueToStore = "fun  text here";
+            basic_string.SetItem(stringValueToStore);
+
+            Console.WriteLine("Stored {0}, Print() tells us:", stringValueToStore);
+            basic_string.Print();
+
+            string stringCheckVal = basic_string.GetItem();
+            Console.WriteLine("Using Get, got back: {0}", stringCheckVal);
+
+            // != is ok for strings in C#, but not in Java
+            if (stringCheckVal != stringValueToStore)
+                Console.WriteLine("\tERROR: checkVal is not {0}", stringValueToStore);
+            else
+                Console.WriteLine("\tGetItem appeared to work!");
+        }
+    }
+
+    // please put your 'BasicGeneric' class here
+    class BasicGeneric<TypeOfData>
+    {
+        // Changes primitive type based on input
+        private TypeOfData storedItem;
+        public void SetItem(TypeOfData d)
+        {
+            storedItem = d;
+        }
+
+        public TypeOfData GetItem()
+        {
+            return storedItem;
+        }
+        public void Print()
+        {
+            Console.WriteLine(storedItem);
+        }
+    }
+    class Basic_AbsValComparer_Test_Code
+    {
+        public void RunExercise()
+        {
+            AbsValComparer absolute_val_comp = new AbsValComparer();
+
+            List<double> nums = new List<double>();
+
+            nums.Add(20.4);
+            nums.Add(-20.4);
+            nums.Add(-10.3);
+            nums.Add(3.1);
+            nums.Add(-4.2);
+
+            Console.WriteLine("Before sorting:\n");
+            foreach (double num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            nums.Sort(absolute_val_comp);
+
+            Console.WriteLine("\nAfter sorting\n");
+
+            foreach (double num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            double[] nums_to_find = { 3.1, -4.2, -20.4, 999 };
+            foreach (double targetNum in nums_to_find)
+            {
+                int loc = nums.BinarySearch(targetNum, absolute_val_comp);
+                if (loc >= 0)
+                    Console.WriteLine("Found {0} at location {1}", targetNum, loc);
+                else
+                    Console.WriteLine("Did not find {0}", targetNum);
+            }
+        }
+    }
+
+    // please put your 'AbsValComparer' class here
+    class AbsValComparer : IComparer<double>
+    {
+        public int Compare(double x, double y)
+        {
+            if (Math.Abs(x) < Math.Abs(y))
+                return -1;
+            else if (Math.Abs(y) < Math.Abs(x))
+                return 1;
+            else if (Math.Abs(x) == Math.Abs(y) && x < 0 && y > 0)
+                return -1;
+            else if (Math.Abs(x) == Math.Abs(y) && y < 0 && x > 0)
+                return 1;
+            else
+                return 0;
+        }
+    }
+
+    class What_Is_An_Enum
+    {
+        // What is an enum? Why would you want to use one?
+        /// An enum (short for enumeration) in C#, is a keyword that represents a value type for declaring a set of
+        /// named integer constants. It can be defined using the enum keywork directly inside a namespace, class, or structure
+        /// The enum is used to give a name to each constant so that the constant integer can be referred using its name.
+ 
+               
+        // Why is an enum better than a public const int? (or a public static readonly int)?
+        /// Because, unlike the int, it makes code more readable by giving related constants meaningful names.
+        /// it also simultaneously improves maintainability by giving developers a simple reference for constants.
+    }
+}
+
---
+++ lesson-2/Patrick_OBrien_SUBMISSION/PCE_02/SmartArray_Enums_Test/Program.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using SmartArray_Test;
+
+// 400_enum_ErrorCode Project
+
+namespace SmartArray_Test
+{
+
+   // Your enum goes here:
+    enum ErrorCode
+    {
+        NoError,
+        Underflow,
+        Overflow
+    }
+
+    
+    class SmartArray
+    {
+        int[] rgNums;
+
+        //// Running time for this method: 
+        /// O(1) - Constant time
+        //// Explanation (Why did you choose that running time?):
+        /// I chose this running time because there is only one element that remains
+        /// the same, there will always be the single step of creating the array that is size 5. 
+        public SmartArray()
+        {
+           rgNums = new int[5];
+        }
+
+        //// Running time for this method:
+        /// O(N) - Linear Time
+        //// Explanation (Why did you choose that running time?):
+        /// Instead of an unchanging int like the default SmartArray constructor, the howMany parameter can be
+        /// any size. For this reason the time is linear instead of constant. 
+        /// Because memory allocation takes longer time for larger arrays.
+        /// ex:
+        /// around 10 ns for an array of size 1
+        /// around 400 ns for an array of size 10,000
+        /// around 300,000 ns for an array of size 1,000,000
+        
+        public SmartArray(int howMany)
+        {
+            // Second overloaded constructor
+            // An integer that specifies what the size of the array should be
+            // Allocates an array given by the param
+
+            rgNums = new int[howMany];
+        }
+
+        // Running time for this method:
+        /// O(1) - Constant time
+        // Explanation (Why did you choose that running time?):
+        /// Given that each are conditional statements, where only one can be met, this method is constant time.
+        /// It can be an Underflow, but not an Overflow, and it can have no error, but then it would be neither
+        /// an Underflow or an Overflow.
+        
+        public ErrorCode SetAtIndex(int idx, int val)
+        {
+            // set the value at the index
+            if (idx < 0)
+            {
+                return ErrorCode.Underflow;
+            }           
+
+            else if (idx > rgNums.Length || rgNums.Length == idx)
+            {
+                return ErrorCode.Overflow;
+            }
+            // check to make sure the index is set
+            // set rgNums[idx] out as val
+            rgNums[idx] = val;
+
+            if (rgNums[idx] == val)
+            {
+                // if it is, return no error
+                return ErrorCode.NoError;
+            }
+            
+            return ErrorCode.Underflow;
+        }
+
+        // Running time for this method:
+        ///  O(1) - Constant time
+        // Explanation (Why did you choose that running time?):
+        /// There is always a single step to take, the number of steps for this method does not change when the 
+        /// size of the array changes. 
+        /// It will be one of the statements, not more than one. Either it will be:
+        /// (idx < 0) --> Underflow
+        /// or
+        /// (idx > rgNums.Length || rgNums.Length == idx)
+        /// or the val = rgNums[idx]
+
+        public ErrorCode GetAtIndex(int idx, out int val)
+        {
+            // Checks to see if the slot can be accessed
+            if (idx < 0)
+            {
+                val = Int32.MinValue;
+                return ErrorCode.Underflow;
+            }
+            else if (idx > rgNums.Length || rgNums.Length == idx)
+            {
+                val = Int32.MinValue;
+                return ErrorCode.Overflow;
+            }
+            else
+            { 
+                val = rgNums[idx];
+
+                return ErrorCode.NoError;
+            }
+             // return ErrorCode.Overflow;
+        }
+
+        // Running time for this method: 
+        /// O(N) Linear time
+        // Explanation (Why did you choose that running time?):
+        /// We have access to every single element in the array, and we are printing them one at a time.
+        /// There is no special process, so the time is directly correlated to the overall size of the array
+        /// O * N
+        public void PrintAllElements()
+        {
+            for (int i = 0; i < rgNums.Length; i++)
+            {
+                Console.WriteLine(rgNums[i]);
+            }
+        }
+
+
+        // Running time for this method:
+        /// O(N^2) - BubbleSort
+        // Explanation (Why did you choose that running time?):
+        /// This is BubbleSort, we previously discussed that BubbleSort is a linear search algorithm.
+        /// It's a linear search algorithm because we are taking an array, and then searching through it
+        /// one by one until we reach the correct integer. If you look at it on a graph, it appears as a
+        /// linear pattern.
+        /// We increment through the array by one each time, until rgNums[i] == val, and then we end it if 
+        /// it is true, otherwise we return false if the value is not present.
+        public bool Find(int val)
+        {
+            // search the entire length of the array for the value val
+            for (int i = 0; i < rgNums.Length; i++)
+            {
+                if (rgNums[i] == val)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    }
+
+    class Program
+    {
+        // Tests for SmartArrayTests
+        static void Main(string[] args)
+        {
+            SmartArray sa = new SmartArray();
+            const int SMART_ARRAY_SIZE = 5;
+            bool testPassed = false;
+            ErrorCode ec;
+
+            Console.WriteLine("CHECK THIS: SmartArray starts with all zeros");
+            sa.PrintAllElements();
+            Console.WriteLine("\n*******************\n");
+
+
+            Console.WriteLine("================= SetAtIndex =================");
+            Console.WriteLine("AutoChecked: Can add at slot 0?");
+            if ((ec = sa.SetAtIndex(0, 10)) != ErrorCode.NoError)
+                Console.WriteLine("TEST FAILED: UNABLE TO SET ELEMENT 0! (ErrorCode: {0})", ec);
+            else
+                Console.WriteLine("Test Passed: Able to set element 0!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Can add at slots 0-4?");
+            testPassed = true;
+            for (int i = 0; i < SMART_ARRAY_SIZE; i++)
+            {
+                if ((ec = sa.SetAtIndex(i, 10 * i)) != ErrorCode.NoError)
+                {
+                    Console.WriteLine("TEST FAILED: UNABLE TO SET ELEMENT {0}! (ErrorCode: {1})", i, ec);
+                    testPassed = false;
+                    break; // out of the loop
+                }
+            }
+            if (testPassed)
+                Console.WriteLine("Test Passed: Able to set all elements!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to add at slot {0}?", SMART_ARRAY_SIZE);
+
+            if ((ec = sa.SetAtIndex(SMART_ARRAY_SIZE, 10)) != ErrorCode.Overflow)
+                Console.WriteLine("TEST FAILED: SET ELEMENT {0} DIDN'T OVERFLOW (ErrorCode: {1})", SMART_ARRAY_SIZE, ec);
+            else
+                Console.WriteLine("Test Passed: Unable to set element {0}!", SMART_ARRAY_SIZE);
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to add at slot {0}?", SMART_ARRAY_SIZE + 10);
+            if ((ec = sa.SetAtIndex(SMART_ARRAY_SIZE + 10, 10)) != ErrorCode.Overflow)
+                Console.WriteLine("TEST FAILED: SET ELEMENT {0} DIDN'T OVERFLOW (ErrorCode: {1})", SMART_ARRAY_SIZE + 10, ec);
+            else
+                Console.WriteLine("Test Passed: Unable to set element {0}!", SMART_ARRAY_SIZE + 10);
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to add at slot -1?");
+            if ((ec = sa.SetAtIndex(-1, 10)) != ErrorCode.Underflow)
+                Console.WriteLine("TEST FAILED: SET ELEMENT -1 DIDN'T UNDERFLOW! ErrorCode: {0})", ec);
+            else
+                Console.WriteLine("Test Passed: Unable to set element -1!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to add at slot -10?");
+            if ((ec = sa.SetAtIndex(-10, 10)) != ErrorCode.Underflow)
+                Console.WriteLine("TEST FAILED: SET ELEMENT -10 DIDN'T UNDERFLOW! ErrorCode: {0})", ec);
+            else
+                Console.WriteLine("Test Passed: Unable to set element -1!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("CHECK THIS: Should see 0, 10, 20, 30, 40");
+            sa.PrintAllElements();
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("================= GetAtIndex =================");
+            int valueGotten;
+            Console.WriteLine("AutoChecked: Can get from slot 0?");
+            ec = sa.GetAtIndex(0, out valueGotten);
+            if (ec != ErrorCode.NoError)
+            {
+                Console.WriteLine("TEST FAILED: UNABLE TO GET FROM SLOT 0 (ErrorCode: {0})", ec);
+            }
+            else if (valueGotten != 0)
+            {
+                Console.WriteLine("TEST FAILED: UNEXPECTED VALUE FROM SLOT 0: (EXPECTED 0, GOT {0})", valueGotten);
+            }
+            else
+                Console.WriteLine("Test Passed: Able to get expected value from slot 0!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Can get from slots 0-4?");
+            testPassed = true;
+            for (int i = 0; i < SMART_ARRAY_SIZE; i++)
+            {
+                ec = sa.GetAtIndex(i, out valueGotten);
+                if (ec != ErrorCode.NoError)
+                {
+                    Console.WriteLine("TEST FAILED: UNABLE TO GET FROM SLOT {0} (ErrorCode: {1})", i, ec);
+                }
+                else if (valueGotten != 10 * i)
+                {
+                    Console.WriteLine("TEST FAILED:  UNEXPECTED VALUE AT SLOT {0} (EXPECTED {1}, GOT {2})", i, i * 10, valueGotten);
+                    testPassed = false;
+                    break; // out of the loop
+                }
+            }
+            if (testPassed)
+                Console.WriteLine("Test Passed: Able to get expected values!");
+            Console.WriteLine("\n*******************\n");
+
+
+            Console.WriteLine("AutoChecked: Should NOT be able to get from slot {0}?", SMART_ARRAY_SIZE);
+            if ((ec = sa.GetAtIndex(SMART_ARRAY_SIZE, out valueGotten)) != ErrorCode.Overflow)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T OVERFLOW (ErrorCode: {1})?", SMART_ARRAY_SIZE, ec);
+            else if (valueGotten != Int32.MinValue)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T PRODUCE Int32.MinValue", SMART_ARRAY_SIZE);
+            else
+                Console.WriteLine("Test Passed: Unable to get from slot {0}!", SMART_ARRAY_SIZE);
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to get from slot {0}?", SMART_ARRAY_SIZE + 10);
+            if ((ec = sa.GetAtIndex(SMART_ARRAY_SIZE + 10, out valueGotten)) != ErrorCode.Overflow)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T OVERFLOW (ErrorCode: {1})?", SMART_ARRAY_SIZE + 10, ec);
+            else if (valueGotten != Int32.MinValue)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T PRODUCE Int32.MinValue", SMART_ARRAY_SIZE + 10);
+            else
+                Console.WriteLine("Test Passed: Unable to get from slot {0}!", SMART_ARRAY_SIZE + 10);
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to get from slot -1?");
+            if ((ec = sa.GetAtIndex(-1, out valueGotten)) != ErrorCode.Underflow)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T UNDERFLOW(ErrorCode: {1})", -1, ec);
+            else if (valueGotten != Int32.MinValue)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T PRODUCE Int32.MinValue", -1);
+            else
+                Console.WriteLine("Test Passed: Unable to get from slot {0}!", SMART_ARRAY_SIZE);
+
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to get from slot -10?");
+            if ((ec = sa.GetAtIndex(-10, out valueGotten)) != ErrorCode.Underflow)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T UNDERFLOW(ErrorCode:{1})", -10, ec);
+            else if (valueGotten != Int32.MinValue)
+                Console.WriteLine("TEST FAILED: GET FROM ELEMENT {0} DIDN'T PRODUCE Int32.MinValue", -10);
+            else
+                Console.WriteLine("Test Passed: Unable to get from slot {0}!", SMART_ARRAY_SIZE);
+            Console.WriteLine("\n*******************\n");
+
+
+            Console.WriteLine("================= Find =================");
+            Console.WriteLine("AutoChecked: Can find 0?");
+            if (!sa.Find(0))
+                Console.WriteLine("TEST FAILED: UNABLE TO FIND VALUE 0!");
+            else
+                Console.WriteLine("Test Passed: Able to find value 0!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Can find the values in slots 0-4?");
+            testPassed = true;
+            for (int i = 0; i < SMART_ARRAY_SIZE; i++)
+            {
+                ec = sa.GetAtIndex(i, out valueGotten);
+                if (ec != ErrorCode.NoError)
+                {
+                    Console.WriteLine("TEST FAILED:UNABLE TO GET ELEMENT AT SLOT {0} (ErrorCode: {1})!", i, ec);
+                    testPassed = false;
+                    break;
+                }  // technically don't need the 'else'....
+                else if (!sa.Find(valueGotten)) // test by getting from array
+                {
+                    Console.WriteLine("TEST FAILED: UNABLE TO FIND {0}!", valueGotten);
+                    testPassed = false;
+                    break; // out of the loop
+                }
+            }
+            if (testPassed)
+                Console.WriteLine("Test Passed: Able to find values that are already in the array!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Can find the values calculated to be in slots 0-4?");
+            testPassed = true;
+            for (int i = 0; i < SMART_ARRAY_SIZE; i++)
+            {
+                if (!sa.Find(i * 10)) // test by re-calculating the result
+                {
+                    Console.WriteLine("TEST FAILED: UNABLE TO FIND {0}!", i * 10);
+                    testPassed = false;
+                    break; // out of the loop
+                }
+            }
+            if (testPassed)
+                Console.WriteLine("Test Passed: Able to find values calculated to be in the array!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to find -1?");
+            if (sa.Find(-1))
+                Console.WriteLine("TEST FAILED: ABLE TO FIND -1, WHICH SHOULD NOT BE PRESENT");
+            else
+                Console.WriteLine("Test Passed: Unable to find nonexistent value -1!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to find -10?");
+            if (sa.Find(-10))
+                Console.WriteLine("TEST FAILED: ABLE TO FIND -10, WHICH SHOULD NOT BE PRESENT");
+            else
+                Console.WriteLine("Test Passed: Unable to find nonexistent value -10!");
+            Console.WriteLine("\n*******************\n");
+
+            Console.WriteLine("AutoChecked: Should NOT be able to find 11?");
+            if (sa.Find(11))
+                Console.WriteLine("TEST FAILED: ABLE TO FIND 11, WHICH SHOULD NOT BE PRESENT");
+            else
+                Console.WriteLine("Test Passed: Unable to find nonexistent value 11!");
+            Console.WriteLine("\n*******************\n");
+        }
+    }
+//}
