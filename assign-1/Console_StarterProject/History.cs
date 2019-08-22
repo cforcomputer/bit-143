eb0b8df92c53e7107f1d4bdb1648b73d90219ff8
cforcomputer <patjobri@gmail.com>
2019-08-21 23:07:47 -0700

    updated to include corrected comments

--- assign-1/Console_StarterProject/History.cs
+++ assign-1/Console_StarterProject/History.cs
@@ -17,6 +17,9 @@ namespace Helpdesk
 {
     class History //LinkedList
     {
+
+        private Node head;
+
         public class Node
         {
             public Object data { get; set; }
@@ -30,69 +33,128 @@ namespace Helpdesk
             }
         }
 
-        private Node head;
-
-        // Print all elements in the linked list
         public void PrintAll()
         {
-            Node current = head;
-            while (current != null)
+            //Node current = head;
+            //while (current != null)
+            //{
+            //    Console.WriteLine(current.data + " \n");
+            //    current = current.Next;
+            //}
+
+            // CORRECTION: FIRST PRINT THE HISTORY, THEN PRINT THE FUTURE
+            // PRINT FROM THE [MOST RECENT ITEM] AND WORK BACK [TOWARDS THE OLDEST ITEM]
+
+            // A SEPARATE CASE IS NOT REQUIRED FOR ADDING TO THE FRONT OF THE LIST WHEN IT'S
+            // EMPTY, AND THEN ADDING TO THE LIST WHEN IT'S NOT EMPTY. 
+            // YOU CAN ADD TO EITHER LIST IN ABOUT 4 - 5 LINES OF CODE
+            if (head == null)
             {
-                Console.WriteLine(current.data + " \n");
-                current = current.Next;
+                Console.WriteLine("The list is empty!");
+                return;
             }
+
+            Console.WriteLine("\nHistory:\n{0}", head.data);
+            Console.WriteLine("Previously visited pages:\n");
+            Console.WriteLine("Pages in your 'future':\n");
         }
 
         // Move backwards in the linked list
         public void MoveBackwards()
         {
-            Node previous = null, current = head, next;
-            while (current !=null)
+            //Node previous = null, current = head, next;
+            //while (current !=null)
+            //{
+            //    next = current.Next;
+            //    current.Next = previous;
+            //    previous = current;
+            //    current = next;
+            //}
+            //head = previous;
+
+            // DOES NOT WORK, SHOULD NOT REVERSE THE LIST
+
+            // DOES NOT CHECK TO SEE IF THE LIST IS EMPTY - WILL CRASH
+
+            // SHOULD REMOVE THE ITEM FROM THE FRONT OF THE LIST, THEN 
+            // ADD IT TO THE "FUTURE" LIST. CURRENTLY NOT ADDING TO FUTURE LIST.
+
+            // SHOULD REMOVE THE CURRENT NODE, TAKE STRING OUT, THEN CREATE A BRAND NEW NODE
+            // TO STORE THE STRING THAT YOU ADD TO THE FUTURE LIST, INSTEAD OF CREATING A NEW ONE.
+            // FIX SIMILAR PROBLEMS
+
+            // MOVEFORWARDS/BACKWARDS: IF YOU'RE ALREADY AT THE END OF THE FUTURE/HISTORY LIST AND
+            // YOU CALL THESE, THEN YOU'LL ADD A "NULL" STRING TO THE OTHER LIST.
+            if (head == null)
             {
-                next = current.Next;
-                current.Next = previous;
-                previous = current;
-                current = next;
+                return;
             }
-            head = previous;
+            else
+            {
+
+            }
         }
+
+
 
         // Move forwards in the LinkedList
         public void MoveForwards()
         {
-            if (head == null) // list is empty
+            //if (head == null) // list is empty
+            //{
+            //    throw new Exception("You're at the end of the list!");
+            //}
+            //// at least one page in the next node of the list, remove it and then add it to the previously
+            //// visited section of the list (behind it)
+            //else
+            //{
+            //    Node current = head; 
+            //    current.Next = current;                               
+            //}
+
+            // DOES NOT WORK - IT CURRENTLY ALWAYS RESETS THE HISTORY TO BEING THE SECOND ITEM ON THE LIST
+
+            if (head == null)
             {
-                throw new Exception("You're at the end of the list!");
+                return;
             }
-            // at least one page in the next node of the list, remove it and then add it to the previously
-            // visited section of the list (behind it)
             else
             {
-                Node current = head, next = null;
-                // next = current.Next;
-                current.Next = current;
-                // current = next;                
+                head = head.Next;
             }
         }
+
+
 
         // This method adds a new element to the last visited node of the linked list
         public void VisitPage(string pageName) // string is the text description of what the page is
         {
-            //Node dat = null, current = head, next = null;
+            // MUST ALSO REMOVE ALL ENTRIES IN THE "FUTURE" LIST
+            // PUT IN A COMMENT STATING THE RUNNING TIME OF THE CURRENT SOLUTION
+            // THEN IMPLEMENT CODE THAT TAKES A CONSTANT TIME O(1) TO IMPLEMENT THE LIST
+
+            // Running time of the current solutin is O(N) because currently VisitPage checks
+            // every node in the list, no no good!
+
             // adds a node with the visited page information to the linked list
+            //if (head == null)
+            //{
+            //    head = new Node(pageName);
+            //}
+            //else
+            //{
+            //    Node empty = new Node(pageName);
+            //    empty.Next = head;
+            //    head = empty;
+            //}
+            
+
             if (head == null)
             {
                 head = new Node(pageName);
             }
             else
-            {
-                //Node prior = head;
-                //while (prior.Next != null)
-                //{
-                //    prior = prior.Next;
-                //}
-                //prior.Next = new Node(pageName);
-
+            {                
                 Node empty = new Node(pageName);
                 empty.Next = head;
                 head = empty;
